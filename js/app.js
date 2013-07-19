1f9e9bc495a18811173cca56dd4afd8e106a2c23
gekkouksi <gekkoukisi@i.softbank.jp>
2013-07-19 15:43:55 +0900

    003

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@
 	var obj = new Backbone.Model();
 
 	obj.set({name: "Murata"});
-	obj.set({age: 20}:q
+	obj.set({age: 20});
 
 	console.log("obj: " + JSON.stringify(obj));
 	console.log("obj.name: " + obj.get("name"));
